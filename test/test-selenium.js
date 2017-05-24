3b1feea7fb6d0edac19a8ead0006a43ea5502ab6
theftprevention <theftprevention@gmail.com>
2017-05-24 18:13:52 -0400

    Basic testing workflow, attempt 2

--- test/test-selenium.js
+++ test/test-selenium.js
@@ -6,13 +6,15 @@ const
     accessKey = process.env.SAUCE_ACCESS_KEY,
     useSauceConnect = Boolean(username && accessKey);
 
+var browser;
+
 if (useSauceConnect) {
 
     console.log('Using Sauce Connect.');
     console.log('Travis Job Number:   ' + process.env.TRAVIS_JOB_NUMBER);
     console.log('Travis Build Number: ' + process.env.TRAVIS_BUILD_NUMBER);
 
-    let driver = new webdriver.Builder()
+    browser = new webdriver.Builder()
         .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
         .withCapabilities(webdriver.Capabilities.chrome().merge({
             'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
@@ -22,6 +24,10 @@ if (useSauceConnect) {
         }))
         .build();
 
-
+    browser.get('http://localhost:8000/test/index.html').then(function () {
+        browser.getTitle().then(function (title) {
+            console.log('Page title: "' + title + '"');
+        });
+    });
 
 }
