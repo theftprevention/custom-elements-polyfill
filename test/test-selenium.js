a09c6f35a178732869d6a0565b213a7895bf3e11
theftprevention <theftprevention@gmail.com>
2017-05-24 20:29:15 -0400

    Basic testing workflow, attempt 5

--- test/test-selenium.js
+++ test/test-selenium.js
@@ -17,28 +17,47 @@ if (sauce) {
     console.log('Travis Job Number:   ' + process.env.TRAVIS_JOB_NUMBER);
     console.log('Travis Build Number: ' + process.env.TRAVIS_BUILD_NUMBER);
 
-    browser = new webdriver.Builder()
-        .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-        .withCapabilities(webdriver.Capabilities.chrome().merge({
-            'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-            build: process.env.TRAVIS_BUILD_NUMBER,
-            username: username,
-            accessKey: accessKey
-        }))
-        .build();
+    //browser = new webdriver.Builder()
+    //    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    //    .withCapabilities(webdriver.Capabilities.chrome().merge({
+    //        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    //        build: process.env.TRAVIS_BUILD_NUMBER,
+    //        username: username,
+    //        accessKey: accessKey
+    //    }))
+    //    .build();
+    //
+    //browser.getSession().then(function (session) {
+    //    console.log('Session ID is ' + (session && typeof session.getId === 'function' ? '"' + session.getId() + '"' : 'unavailable') + '.');
+    //    return browser.get('http://localhost:8000/test/index.html');
+    //}).then(function () {
+    //    return browser.wait(until.elementLocated(by.id('results')), 80000);
+    //}).then(function (resultContainer) {
+    //    return resultContainer.getText();
+    //}).then(function (resultText) {
+    //    console.log('Results:')
+    //    console.log(resultText);
+    //
+    //    browser.quit();
+    //});
 
-    browser.getSession().then(function (session) {
-        console.log('Session ID is ' + (session && typeof session.getId === 'function' ? '"' + session.getId() + '"' : 'unavailable') + '.');
-        return browser.get('http://localhost:8000/test/index.html');
-    }).then(function () {
-        return browser.wait(until.elementLocated(by.id('results')), 80000);
-    }).then(function (resultContainer) {
-        return resultContainer.getText();
-    }).then(function (resultText) {
-        console.log('Results:')
-        console.log(resultText);
-
-        browser.quit();
+    console.log('Starting manual browser tests');
+    sauce.send({
+        method: 'POST',
+        path: ':username/js-tests',
+        data: {
+            url: 'http://www.jacobmccollum.com/custom-elements-polyfill/',
+            platforms: [
+                ['Windows 7', 'chrome', '58']
+            ],
+            framework: 'mocha'
+        }
+    }, function (err, response) {
+        if (err) {
+            throw err;
+        }
+        console.log('API response:');
+        console.log(response);
     });
 
     console.log('End of script');
